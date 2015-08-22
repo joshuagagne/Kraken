6fe7741848189937e8ba1f17f0b9f7cb035fe8e0
joshuagagne <jag9498@gmail.com>
2015-08-22 18:21:09 -0400

    New Menu
    new menu and scene

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneLoader : MonoBehaviour
+{
+
+	public void LoadLevel ()
+	{
+		Debug.Log ("Load");
+		Application.LoadLevel ("kraken");
+	}
+}
